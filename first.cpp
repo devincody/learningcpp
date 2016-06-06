e10a4b4f6dfa33f0251f21061068993d5dc889e6
Devin Cody <devincody@gmail.com>
2016-06-05 23:04:33 -0700

    first program complete

--- first.cpp
+++ first.cpp
@@ -1,5 +1,16 @@
-#import <stdio.h>
+
+#include <cstdio>
+#include <cmath>
+#include <iostream>
+using namespace std;
 
 int main(){
+	double a;
+
+	a = 1.2;
+	a = sin(a);
+	printf("%6.3f\n", a);
+	//cout << a <<endl;
+
 	return 0;
 }
